b70c0005a9baa969753bc343837530db5294a0e0
mariel <coutomariel@gmail.com>
2021-11-24 08:58:18 -0300

    Swagger configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
     implementation("mysql:mysql-connector-java")
     implementation("org.flywaydb:flyway-core")
 
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
