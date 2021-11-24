ae18e585a0ba363f7870fefb69a48046e17b8391
mariel <coutomariel@gmail.com>
2021-11-24 16:29:07 -0300

    create purchase's model and migration

---
+++ src/main/java/com/mercadolivro/model/PurchaseModel.kt
@@ -0,0 +1,25 @@
+package com.mercadolivro.model
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.*
+
+@Entity(name = "purchase")
+data class PurchaseModel(
+    @Id
+    var id: UUID? = UUID.randomUUID(),
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    val customerModel: CustomerModel,
+
+    @ManyToMany
+    @JoinTable(name = "purchase_book",
+        joinColumns = [JoinColumn(name = "purchase_id")],
+        inverseJoinColumns = [JoinColumn(name = "book_id")]
+    )
+    val books: List<BookModel>,
+    val nfe: String? = null,
+    val price: BigDecimal,
+    val createdAt: LocalDateTime,
+)
---
+++ src/main/resources/db/migration/V004__create_table_purchase_and_purchase_book.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `purchase` (
+    `id` BINARY(16) PRIMARY KEY,
+    `customer_id` BINARY(16) NOT NULL,
+    `nfe` VARCHAR(255) NOT NULL,
+    `price` DECIMAL(10,2) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+)  ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS `purchase_book` (
+    `purchase_id` BINARY(16) NOT NULL,
+    `book_id` BINARY(16) NOT NULL,
+    FOREIGN KEY (purchase_id) REFERENCES purchase(customer_id),
+    FOREIGN KEY (book_id) REFERENCES book(id),
+    PRIMARY KEY (purchase_id, book_id)
+)  ENGINE=INNODB;
